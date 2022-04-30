da2a4bc57a02dd02e076477ec13dd7d734c8498f
dealdreygaroeb@gmail.com <dealdreygaroeb@gmail.com>
2022-04-30 13:41:15 +0200

    upd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 body.html("<bar><input type=text placeholder='AudioLuv - search'/></bar><button>hello</button>")
 body.query({type:"button"}).attr("onclick",e=>body.add("<p>hi</p>"))
 x=new XMLHttpRequest()
-x.open("GET","http://google.com")
+x.open("GET","https://www.google.com")
 x.send()
 x.onload=console.log
