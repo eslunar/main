744be6b088d9f8216a3ae248a5edf71da16d7966
dealdreygaroeb@gmail.com <dealdreygaroeb@gmail.com>
2022-06-04 14:41:17 +0200

    mod

--- ui/helper/modal.js
+++ ui/helper/modal.js
@@ -1,5 +1,5 @@
 function Modal(child,css={}){
-  this.element=new Element(`<modal uuid="${app.uuid(100)}" style="background:#00000025;top:0;left:0;width:100vw;height:100vh;z-index:3;position:fixed;display:flex;align-items:center;align-content:center;justify-content:center;"></modal>`).css(css)
+  this.element=new Element(`<modal uuid="${app.uuid(100)}" style="background:#00000025;top:0;left:0;bottom:0;right:0;z-index:3;position:fixed;display:flex;align-items:center;align-content:center;justify-content:center;"></modal>`).css(css)
   this.element.on("click",function(e){e.target==this.raw?app.back():""})
   
   typeof child=="string"?this.element.html(child):this.element.add(child)
