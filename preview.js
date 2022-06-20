9d0f8372bf3b9bdf8423454365bb32f3d4a3391a
dealdreygaroeb@gmail.com <dealdreygaroeb@gmail.com>
2022-06-20 17:04:22 +0200

    nullattr

--- preview.js
+++ preview.js
@@ -1,2 +1,2 @@
 //arc=await script("arcadia")
-body.add(new Element("<button>hi").on("click",e=>new Page("preview").open()))
+body.add(new Element("<button b e en>hi").on("click",e=>new Page("preview").open()))
--- ui/element/attr.js
+++ ui/element/attr.js
@@ -1,7 +1,8 @@
 Element.prototype.attr=function(prop,val){
   if(typeof prop=="object")Object.keys(prop).forEach(e=>this.attr(e,prop[e]))
   else if(prop&&val!==undefined){
-    if(prop.startsWith("on"))this.on(prop,val)
+    if(val===null)this.raw.removeAttribute(prop)
+    else if(prop.startsWith("on"))this.on(prop,val)
     else {if(this.raw.hasAttribute(prop))this.raw[prop]=val
     else this.raw.setAttribute(prop,val)}
   } else if(prop&&val===undefined)return prop in this.raw?this.raw[prop]||"":this.raw.getAttribute(prop)||""
